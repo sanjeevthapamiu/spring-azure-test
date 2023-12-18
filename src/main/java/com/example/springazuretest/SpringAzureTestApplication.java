3ec112b9a7f8d97d339a545e4761c7662e558a2d
Sanjeev Thapa <sanjeev.thapa@miu.edu>
2023-12-18 17:07:31 -0600

    Intial Commit

---
+++ src/main/java/com/example/springazuretest/SpringAzureTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springazuretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureTestApplication.class, args);
+	}
+
+}
